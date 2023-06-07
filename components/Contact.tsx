3ab74cd6f26e5a382c402c6d1a64a58d52393c1b
la-machine <frankpythagore@gmail.com>
2023-06-07 15:28:50 +0100

    cantact updates

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -6,11 +6,11 @@ const Contact = () => {
         className="max-w-contentContainer mx-auto py-10 xl:py-32 flex flex-col gap-4
         items-center justify-center">
             <p className="font-titleFont text-lg text-textGreen font-semibold
-            flex items-center tracking-wide">What's Next</p>
+            flex items-center tracking-wide">What is Next </p>
             <h2 className="font-titleFont text-5xl font-semibold "> Get In Touch</h2>
             <p className="max-w-[600px] text-center text-textDark">
                 Am open for any project and business opportunities. My inbox is always open for any
-                problems or if you just want to greet am open don't hesitate.
+                problems or if you just want to greet am open do not hesitate.
             </p>
             <a href="mailto:frankpythagore@gmail.com" target="_blank">
                 <button className="w-40 h-14 border border-textGreen mt-6 font-titleFont
