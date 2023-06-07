bc9ae1cb1ac709c145bc298ffe698bde5c806924
la-machine <frankpythagore@gmail.com>
2023-06-07 16:08:53 +0100

    image updates

--- public/asset/index.ts
+++ public/asset/index.ts
@@ -1,9 +1,9 @@
 import form from "./images/form.png";
-import project1 from "./images/*.png";
+import project1 from "./images/project1.jpg";
 import logo1 from "./images/logo1.jpg";
 import YFlogo from "./images/YFlogo.png";
 import myProfile from "./images/myProfile.png";
-import swing from "./images/swing.png";
+import swing from "./images/swing.jpg";
 
 
 export {form, project1, logo1, YFlogo,myProfile, swing};
